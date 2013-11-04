cef9525a23688afcb5e6f35294efda2ee507c7bf
scottsimock <simocks@gmail.com>
2013-11-03 20:04:12 -0500

    7.6

--- chimera/ch07/cards.erl
+++ chimera/ch07/cards.erl
@@ -1,5 +1,5 @@
 -module(cards).
--export([make_deck/0, show_deck/1]).
+-export([make_deck/0, show_deck/1, shuffle/1]).
 
 make_deck() ->
 	[ {Card, Suit} ||
@@ -9,3 +9,17 @@ make_deck() ->
 
 show_deck(Deck) ->
 	lists:foreach(fun(Item) -> io:format("~p~n", [Item]) end, Deck).
+
+%% @doc Function will randomly shuffle a deck of cards 
+shuffle(Deck) -> shuffle(Deck, []).
+
+%% If there are no more cards in the original deck, just return the shuffled deck
+shuffle([], Acc) -> Acc;
+
+%% Randomly split the original deck into two lists.
+%% Retain the first list and pop off the Head of the second list
+%% Add the Tail of the second list to the first and add the Head to the shuffled list
+shuffle(List, Acc) ->
+	{Leading, [H | T]} = lists:split(random:uniform(length(List)) -1, List),
+	shuffle(Leading ++ T, [H | Acc]).
+
